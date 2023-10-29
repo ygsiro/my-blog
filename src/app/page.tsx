a1a336ba008ff8da50c4f20a3c93d657d51feaac
ygsiro <entoyukari@gmail.com>
2023-10-29 11:30:01 +0900

    slug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,13 @@ import styles from './page.module.scss'
 
 export default function Home() {
   return (
-    <main></main>
+    <div>
+      <h1>新着記事</h1>
+      <ul>
+        <li>記事１</li>
+        <li>記事２</li>
+        <li>記事３</li>
+      </ul>
+    </div>
   )
 }
